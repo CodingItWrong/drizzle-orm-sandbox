85777cf57c6a7bdb9871deb4c6b79d32f04f0a5a
Josh Justice <me@codingitwrong.com>
2024-02-10 08:10:59 -0500

    Separate out seeding

--- src/seed.ts
+++ src/seed.ts
@@ -1,14 +1,16 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
+import { user } from "./schema";
 
 const connectionString = "postgres://josh:@localhost:5432/drizzle"
 const sql = postgres(connectionString, { max: 1 })
-const db = drizzle(sql, { schema });
+const db = drizzle(sql);
 
 (async () => {
-  const result = await db.query.user.findMany();
-  console.log(result);
+  await db.delete(user);
+  await db.insert(user).values({
+    name: "josh"
+  });
 
   await sql.end();
 })();
